3e9c21baa71b34400db742e92291bc5dd81e1fde
Gloria Rodriguez <gloriarodrife@gmail.com>
2026-01-12 18:03:28 -0500

    feat: Added column styles and button

--- src/components/DayColumn.tsx
+++ src/components/DayColumn.tsx
@@ -5,10 +5,22 @@ import { Day } from '@/app/types';
 
 export default function DayColumn({ name, number }: Day) {
   return (
-    <section>
-      <h1>{name}</h1>
-      <h2>{number}</h2>
-      <div>Tareas...</div>
+    <section className="column-container">
+      <div className="column-header">
+        <div className="day-name">
+          {name} {number}
+        </div>
+        <button className="add-task-btn">+</button>
+      </div>
+      <div>
+        <div> task</div>
+        <div> task</div>
+        <div> task</div>
+        <div> task</div>
+        <div> task</div>
+        <div> task</div>
+        <div> task</div>
+      </div>
     </section>
   );
 }
